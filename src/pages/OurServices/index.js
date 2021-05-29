92ccfcea37734df908012831a38ff15c053944d8
Agostina Caffaratti <agostina.caffaratti@globant.com>
2021-05-29 12:00:17 -0300

    our services desktop

--- src/pages/OurServices/index.js
+++ src/pages/OurServices/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Hero from '../../components/hero'
 import Layout from '../../components/Layout'
 import data from '../../data/BussinesProcess.json'
 import './style.scss'
@@ -6,12 +7,18 @@ import './style.scss'
 const OurServices = () => {
   return (
     <Layout>
-      {data.map((e) => {
+    <div className='ourServicesHero'>
+    <Hero />
+    </div>
+      {data.map((e, index) => {
         return (
-          <div className="service-desc">
+          <div className="service-desc">  
+          <img src={process.env.PUBLIC_URL + '/assets/' + e.img} alt={e.img} />     
+          <div className={'text-' + index}>
             <h3>{e.title}</h3>
             <p className="service-desc__subtitle">{e.subtitle}</p>
             <p>{e.description}</p>
+          </div>   
           </div>
         )
       })}
